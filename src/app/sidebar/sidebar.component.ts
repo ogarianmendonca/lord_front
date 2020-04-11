2f9973293d69e30e2a70a45257ddf1789a2bb4c0
Ogarian Macedo <ogarianmetal@gmail.com>
2020-04-10 22:52:41 -0300

    Iniciando desenvolvimento do front

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
 export interface RouteInfo {
     path: string;
     title: string;
@@ -9,14 +8,14 @@ export interface RouteInfo {
 }
 
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard',     title: 'Dashboard',         icon:'nc-bank',       class: '' },
-    { path: '/icons',         title: 'Icons',             icon:'nc-diamond',    class: '' },
-    { path: '/maps',          title: 'Maps',              icon:'nc-pin-3',      class: '' },
-    { path: '/notifications', title: 'Notifications',     icon:'nc-bell-55',    class: '' },
-    { path: '/user',          title: 'User Profile',      icon:'nc-single-02',  class: '' },
-    { path: '/table',         title: 'Table List',        icon:'nc-tile-56',    class: '' },
-    { path: '/typography',    title: 'Typography',        icon:'nc-caps-small', class: '' },
-    { path: '/upgrade',       title: 'Upgrade to PRO',    icon:'nc-spaceship',  class: 'active-pro' },
+    { path: '/dashboard',     title: 'Dashboard',         icon: 'nc-bank',       class: '' },
+    { path: '/user',          title: 'User Profile',      icon: 'nc-single-02',  class: '' },
+    { path: '/icons',         title: 'Icons',             icon: 'nc-diamond',    class: '' },
+    { path: '/maps',          title: 'Maps',              icon: 'nc-pin-3',      class: '' },
+    { path: '/notifications', title: 'Notifications',     icon: 'nc-bell-55',    class: '' },
+    { path: '/table',         title: 'Table List',        icon: 'nc-tile-56',    class: '' },
+    { path: '/typography',    title: 'Typography',        icon: 'nc-caps-small', class: '' },
+    { path: '/upgrade',       title: 'Upgrade to PRO',    icon: 'nc-spaceship',  class: 'active-pro' },
 ];
 
 @Component({
